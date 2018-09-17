d5fa660910843d2d104e22988b44dce41ce50342
Irving E Rabel <rabel606@gmail.com>
2018-09-17 20:02:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(fizz_3)
+  fizz_3 = "Fizz"
+  3/3 == fizz_3
+  fizz_5 = "Buzz"
+  5/5== fizz_5
+  fizz_15 = "FizzBuzz"
+  15/3 || 15/5 == fizz_15
+  fizz_4 = puts 
+  4/5 || 4/3 == fizz_4
+  
+end
