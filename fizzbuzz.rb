868fbff4b62917fc6a15fca466b9582f9db1eff9
Irving E Rabel <rabel606@gmail.com>
2018-09-17 20:40:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz_3)
-  fizz_3 = "Fizz"
-  3/3 == fizz_3
-  fizz_5 = "Buzz"
-  5/5== fizz_5
-  fizz_15 = "FizzBuzz"
-  15/3 || 15/5 == fizz_15
-  fizz_4 = puts 
-  4/5 || 4/3 == fizz_4
-  
+  if fizz_3 = 3/3
+  return "Fizz"
+  elsif fizz_5 = 5/5
+  return "Buzz"
+  elsif fizz_15 = 15/3 || fizz_15 = 15/5
+  return "FizzBuzz"
+  elsif fizz_4 = 4/5 || 4/3 
+end
 end
